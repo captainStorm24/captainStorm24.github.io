8ef346f058c37e194c4b7d6997610b27155bb726
captainStorm24 <127136079+captainStorm24@users.noreply.github.com>
2023-03-07 19:38:19 +0200

    Add files via upload

--- js/navbar_hide.js
+++ js/navbar_hide.js
@@ -3,8 +3,12 @@ window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
-  } else {
+    console.log("current" + currentScrollPos + ' ');
+    console.log("prev" + prevScrollpos + ' ');
+  } else if (currentScrollPos!= 0) {
     document.getElementById("navbar").style.top = "-50px";
+    console.log("current" + currentScrollPos + ' ');
+    console.log("prev" + prevScrollpos + ' ');
   }
   prevScrollpos = currentScrollPos;
 }
